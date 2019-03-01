4e4ae7b3920c7f618fbdc17825c8838d8aa7920c
arun.v <arun.v@quikr.com>
2019-03-01 15:26:03 +0530

    dashboard changes are done

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
   <title>SB Admin 2 - Charts</title>
 
   <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="{!! asset('css/all.min.css') !!}" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
@@ -365,7 +365,6 @@
                     <canvas id="myBarChart"></canvas>
                   </div>
                   <hr>
-                  Styling for the bar chart can be found in the <code>/js/demo/chart-bar-demo.js</code> file.
                 </div>
               </div>
 
@@ -437,22 +436,20 @@
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="{!! asset('js/jquery.min.js') !!}"></script>
+  <script src="{!! asset('js/bootstrap.bundle.min.js') !!}"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="{!! asset('js/jquery.easing.min.js') !!}"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
+  <script src="{!! asset('js/sb-admin-2.min.js') !!}"></script>
 
   <!-- Page level plugins -->
-  <script src="vendor/chart.js/Chart.min.js"></script>
+  <script src="{!! asset('js/Chart.min.js') !!}"></script>
 
   <!-- Page level custom scripts -->
-  <script src="js/demo/chart-area-demo.js"></script>
-  <script src="js/demo/chart-pie-demo.js"></script>
-  <script src="js/demo/chart-bar-demo.js"></script>
-
+  <script src="{!! asset('js/chart-area-demo.js') !!}"></script>
+  <script src="{!! asset('js/chart-pie-demo.js') !!}"></script>
 </body>
 </html>
