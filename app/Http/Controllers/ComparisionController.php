377f74a3d8a92fc949bb20d888e32fc19c17d214
Suman Kumar Das <suman.das@quikr.com>
2019-03-01 18:30:59 +0530

    Fix

--- app/Http/Controllers/ComparisionController.php
+++ app/Http/Controllers/ComparisionController.php
@@ -34,15 +34,13 @@ class ComparisionController extends Controller
                     "title" => "Sobha City",
                     "image" => "i6/20180711/2025-Sq-ft-Residential-Villa-for-rent-in-Kaza--Guntur-VB201705171774173-ak_LWBP612469700-1531287282nr424x318sm124x93sq88x66lg728x546gv262x175.png",
                     "fields" => [
-                        [
-                            "Price" => 1200000,
-                            "Area (in Sqft)" => 1300,
-                            "Address" => "Thanisandra, Bangalore",
-                            "Available From" => "10 March 2019",
-                            "Furnished" => true,
-                            "Registration Charges" => 150000,
-                            "Carpet Area" => 1200
-                        ]
+                        "Price" => 1200000,
+                        "Area (in Sqft)" => 1300,
+                        "Address" => "Thanisandra, Bangalore",
+                        "Available From" => "10 March 2019",
+                        "Furnished" => true,
+                        "Registration Charges" => 150000,
+                        "Carpet Area" => 1200
                     ]
                 ],
                 [
@@ -50,15 +48,13 @@ class ComparisionController extends Controller
                     "title" => "RES Residency",
                     "image" => "i6/20180711/2025-Sq-ft-Residential-Villa-for-rent-in-Kaza--Guntur-VB201705171774173-ak_LWBP612469700-1531287282nr424x318sm124x93sq88x66lg728x546gv262x175.png",
                     "fields" => [
-                        [
-                            "Price" => 1200000,
-                            "Area (in Sqft)" => 1300,
-                            "Address" => "Thanisandra, Bangalore",
-                            "Available From" => "10 March 2019",
-                            "Furnished" => true,
-                            "Registration Charges" => 150000,
-                            "Carpet Area" => 1200
-                        ]
+                        "Price" => 1200000,
+                        "Area (in Sqft)" => 1300,
+                        "Address" => "Thanisandra, Bangalore",
+                        "Available From" => "10 March 2019",
+                        "Furnished" => true,
+                        "Registration Charges" => 150000,
+                        "Carpet Area" => 1200
                     ]
                 ],
                 [
@@ -66,15 +62,13 @@ class ComparisionController extends Controller
                     "title" => "Manyata Residency",
                     "image" => "i6/20180711/2025-Sq-ft-Residential-Villa-for-rent-in-Kaza--Guntur-VB201705171774173-ak_LWBP612469700-1531287282nr424x318sm124x93sq88x66lg728x546gv262x175.png",
                     "fields" => [
-                        [
-                            "Price" => 19000000,
-                            "Area (in Sqft)" => 1300,
-                            "Address" => "Nagawara, Bangalore",
-                            "Available From" => "18 March 2019",
-                            "Furnished" => true,
-                            "Registration Charges" => 150000,
-                            "Carpet Area" => 1500
-                        ]
+                        "Price" => 19000000,
+                        "Area (in Sqft)" => 1300,
+                        "Address" => "Nagawara, Bangalore",
+                        "Available From" => "18 March 2019",
+                        "Furnished" => true,
+                        "Registration Charges" => 150000,
+                        "Carpet Area" => 1500
                     ]
                 ]
             ]
